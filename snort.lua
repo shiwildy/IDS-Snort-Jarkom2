01ae673f04630ea5b7b257e61110f489ee3b2402
Wildy Sheverando <shiwildy@gmail.com>
2025-03-31 18:28:35 +0700

    feat: snort config

---
+++ snort.lua
@@ -0,0 +1,24 @@
+ips = {
+   mode = tap,
+   variables = default_variables,
+   rules = [[
+        include /etc/snort/rules/local.rules
+   ]]
+}
+
+alert_fast = {
+   file = true,
+   packet = false
+}
+
+alert_csv = {
+   file = true,
+   separator = ",",
+   fields = "timestamp proto src_addr src_port dst_addr dst_port msg class sid" 
+}
+
+config = {
+   detection = {
+      flow_timeout = 30
+   }
+}
