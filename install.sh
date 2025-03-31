ee91899585f59e42fc81f281353b300ba953fa32
Wildy Sheverando <shiwildy@gmail.com>
2025-03-31 14:58:35 +0700

    feat: create installation script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! [[ $(cat /etc/os-release | grep -w ID | sed 's/ID=//g' | head -n1) ==  'debian' ]]; then
+  echo "Installer ini dibuat untuk OS debian"
+  exit
+fi
+
+# >> Update repository
+apt update -y; apt upgrade -y
+
+# >> Install required packages
+apt install build-essential cmake libpcap-dev libpcre3-dev zlib1g-dev libluajit-5.1-dev libssl-dev pkg-config git tshark net-tools -y
+
+# >> Install snort
+git clone https://github.com/snort3/snort3.git
+cd snort3
+./configure_cmake.sh --prefix=/usr/local/snort
+cd build
+make -j$(nproc)
+sudo make install
+
+# >> Salin Config default
+mkdir -p /etc/snort/rules
+cp -r etc/* /etc/snort/
