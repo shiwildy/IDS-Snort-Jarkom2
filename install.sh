1ffd54730cb1c31678cba1f38a3329dfaf25e930
Wildy Sheverando <shiwildy@gmail.com>
2025-03-31 15:09:19 +0700

    feat: add libdaq installation

--- install.sh
+++ install.sh
@@ -9,16 +9,28 @@ fi
 apt update -y; apt upgrade -y
 
 # >> Install required packages
-apt install build-essential cmake libpcap-dev libpcre3-dev zlib1g-dev libluajit-5.1-dev libssl-dev libdaq-dev pkg-config git tshark net-tools -y
+apt install build-essential cmake libpcap-dev libpcre3-dev zlib1g-dev libluajit-5.1-dev libssl-dev autoconf pkg-config git tshark net-tools -y
+
+# >> Install libdaq
+git clone https://github.com/snort3/libdaq.git
+cd libdaq
+./bootstrap
+./configure
+make -j $(nproc)
+make install
+ldconfig # >> update ldconfig
+cd /root/ && rm -rf libdaq
 
 # >> Install snort
 git clone https://github.com/snort3/snort3.git
 cd snort3
 ./configure_cmake.sh --prefix=/usr/local/snort
 cd build
-make -j$(nproc)
+make -j $(nproc)
 make install
 
 # >> Salin Config default
 mkdir -p /etc/snort/rules
 cp -r etc/* /etc/snort/
+cd /root/ && rm -rf snort3
+
