f97d4c7b4603d8a390652b02bc7bb58cb2545aaf
Wildy Sheverando <shiwildy@gmail.com>
2025-03-31 18:30:54 +0700

    feat: update installer

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt update -y; apt upgrade -y
 
 # >> Install required packages
 apt install build-essential cmake libpcap-dev libpcre3-dev zlib1g-dev libluajit-5.1-dev ethtool libssl-dev automake libtool flex liblzma-dev \
-libhwloc-dev libpcre2-dev autoconf pkg-config git tshark net-tools -y
+libhwloc-dev libpcre2-dev autoconf pkg-config git tshark net-tools screen -y
 
 # >> Install libdaq
 git clone https://github.com/snort3/libdaq.git
@@ -30,13 +30,13 @@ cd build
 make -j $(nproc)
 make install
 
-# >> Salin Config default
+# >> Buat folder untuk snort
+mkdir -p /etc/snort/
 mkdir -p /etc/snort/rules
+mkdir -p /etc/snort/log
 cd /root/ && rm -rf snort3
 
-# >> Create snort log directory
-mkdir -p /var/log/snort
-chown -R root:root /var/log/snort
-chmod -R 755 /var/log/snort
-
+# >> Download snort config
+wget -O /etc/snort/snort.lua 'https://raw.githubusercontent.com/shiwildy/IDS-Snort-Jarkom2/refs/heads/main/snort.lua'
+wget -O /etc/snort/rules/local.rules 'https://raw.githubusercontent.com/shiwildy/IDS-Snort-Jarkom2/refs/heads/main/local.rules'
 
